e012fcfaad88bcaf07a69551e5406406514aae36
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-04-07 22:13:56 -0300

    implementar thread para socket de resposta

--- udp_sockets/client.py
+++ udp_sockets/client.py
@@ -11,24 +11,50 @@ clientSocket.sendto(f"GET {filename}".encode(), ADDR)
 
 brute_response = clientSocket.recvfrom(1024)
 message, addr = brute_response
-response = message.decode().split("-|-")
+response = message.decode().split(" ")
 
 
 if response[0] == "ERROR":
     print("Aconteceu um erro: ", response[1])
 
-if response[0] == "OK":
-    file = open(filename, "ab")
+else:
+    if response[0] == "OK":
 
-    n_packets = response[1]
-    buffer_size = response[2]
+        n_packets = response[1]
+        buffer_size = response[2]
+        lost = []
 
-    for i in range(int(buffer_size)):
-        brute_response = clientSocket.recvfrom(1096)
-        message, addr = brute_response
-        response = message.decode().split("-|-")
-        file.write(bytes(response[1][2:-1], encoding="utf-8"))
+        brute_response = clientSocket.recvfrom(int(buffer_size))
+        buffer = []
+        # ...
+        for i in range(0, int(n_packets)):
+            message, addr = brute_response
+            buffer.append(message)
 
+            if message.decode().split()[0] == "END":
+                break
+
+            brute_response = clientSocket.recvfrom(int(buffer_size))
+
+    file = open(filename, "wb")
+    n_digits = len(str(n_packets))
+    for message in buffer:
+        header = message[0 : n_digits + 1]
+        data = message[n_digits + 2 :]
+
+        if i != int(header.decode()):
+            print(i)
+            lost.append(i)
+
+        print(data)
+        # text = " ".join(response[1:])
+
+        # print(f"{text=}")
+        # clean_text = text.replace("b'", "").replace("'", "").replace('b"', "")
+        # print(f"{clean_text=}")
+        file.write(data[1:-1])
+        # file.write(bytes(clean_text, encoding="utf-8"))
+
+    print(lost)
     file.close()
-
 clientSocket.close()
--- udp_sockets/server.py
+++ udp_sockets/server.py
@@ -1,4 +1,7 @@
 from socket import socket, AF_INET, SOCK_DGRAM
+from random import randint
+from time import sleep
+from threading import Thread
 import os
 
 NAME = ""
@@ -6,35 +9,54 @@ PORT = 8065
 
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind((NAME, PORT))
-# serverSocket.listen(1)
 
-while True:
-    message, addr = serverSocket.recvfrom(1024)
+
+def handle_request(message: bytes, addr: str) -> None:
     request = message.decode().split()
 
+    returnSocket = socket(AF_INET, SOCK_DGRAM)
+    returnSocket.bind((NAME, randint(PORT + 1, PORT + 6001)))
+
     if len(request) <= 1:
-        serverSocket.sendto("ERROR-|-Má requisição".encode(), addr)
-        continue
+        returnSocket.sendto("ERROR-|-Má requisição".encode(), addr)
+        return
 
     if request[0] != "GET":
-        serverSocket.sendto("ERROR-|-Método não permitido".encode(), addr)
-        continue
+        returnSocket.sendto("ERROR-|-Método não permitido".encode(), addr)
+        return
 
     filename = request[1]
     if not os.path.exists(os.path.join("./files", filename)):
-        serverSocket.sendto("ERROR-|-Arquivo não encontrado".encode(), addr)
-        continue
+        returnSocket.sendto("ERROR-|-Arquivo não encontrado".encode(), addr)
+        return
 
+    n_packets = 0
     with open(os.path.join("./files", filename), "rb") as file:
-        data = file.read()
-
-    data_length = len(data)
-    n_packets = data_length // 1024 + 1
+        data = file.read(1024)
+        while data:
+            n_packets += 1
+            data = file.read(1024)
 
     n_digits = len(str(n_packets))
 
-    serverSocket.sendto(f"OK-|-{n_packets}-|-{n_digits+3+1024}".encode(), addr)
-    for i in range(n_packets):
-        serverSocket.sendto(
-            f"{i:{'0'}{n_digits}}-|-{data[i * 1024 : (i + 1) * 1024]}".encode(), addr
-        )
+    returnSocket.sendto(f"OK {n_packets} {n_digits+1+1024}".encode(), addr)
+    with open(os.path.join("./files", filename), "rb") as file:
+        i = 0
+        data = file.read(1024)
+        while data:
+            returnSocket.sendto(
+                b" ".join([f"{i:{'0'}{n_digits}}".encode(), data]), addr
+            )
+            i += 1
+
+            data = file.read(1024)
+
+    sleep(1)
+    returnSocket.sendto(b"END", addr)
+
+
+while True:
+    message, addr = serverSocket.recvfrom(1024)
+
+    thread = Thread(target=handle_request, args=(message, addr), daemon=True)
+    thread.start()
