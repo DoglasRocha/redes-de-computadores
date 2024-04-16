811555e03e25598e79b1ce25157067ae693636bd
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-04-15 21:40:23 -0300

    adicionar timeout no socket e tratamento de erro

--- udp_sockets/client.py
+++ udp_sockets/client.py
@@ -6,69 +6,77 @@ PORT = 8065
 ADDR = (SERVERNAME, PORT)
 
 clientSocket = socket(AF_INET, SOCK_DGRAM)
+clientSocket.settimeout(120)
 
 filename = input("Qual o nome do arquivo que você deseja receber? ")
 packet_to_drop = input("Qual pacote deseja jogar fora? (-1 para não jogar) ")
-clientSocket.sendto(f"GET {filename}".encode(), ADDR)
+try:
+    clientSocket.sendto(f"GET {filename}".encode(), ADDR)
 
-brute_response = clientSocket.recvfrom(1024)
-message, addr = brute_response
+    brute_response = clientSocket.recvfrom(1024)
+    message, addr = brute_response
 
-n_packets = None
-if message[0:5] == b"ERROR":
-    response = message.decode().split(" ")
-    print("Aconteceu um erro: ", " ".join(response[1:]))
-
-else:
-    if message[0:2] == b"OK":
-
+    n_packets = None
+    if message[0:5] == b"ERROR":
         response = message.decode().split(" ")
-        n_packets = response[1]
-        buffer_size = response[2]
-        buffer = []
-        lost = []
+        print("Aconteceu um erro: ", " ".join(response[1:]))
 
-        brute_response = clientSocket.recvfrom(int(buffer_size))
+    else:
+        if message[0:2] == b"OK":
 
-        for i in range(0, int(n_packets)):
-            message, addr = brute_response
-            if message[0:3] == b"END":
-                print("END")
-                break
-
-            if i != int(packet_to_drop):
-                buffer.append(message)
+            response = message.decode().split(" ")
+            n_packets = response[1]
+            buffer_size = response[2]
+            buffer = []
+            lost = []
 
             brute_response = clientSocket.recvfrom(int(buffer_size))
 
-    if n_packets is not None:
-        file_array = [None for i in range(int(n_packets))]
+            for i in range(0, int(n_packets)):
+                message, addr = brute_response
+                if message[0:3] == b"END":
+                    print("END")
+                    break
 
-        n_digits = len(str(n_packets))
-        hash_init = n_digits + 1
-        hash_end = hash_init + 16
-        for packet in buffer:
-            header = packet[0:n_digits]
-            hash_ = packet[hash_init:hash_end]
-            data = packet[hash_end + 1 :]
+                if i != int(packet_to_drop):
+                    buffer.append(message)
 
-            if md5(data).digest() == hash_:
-                file_array[int(header)] = data
+                brute_response = clientSocket.recvfrom(int(buffer_size))
 
-        file = open(filename, "wb")
-        for index, segment in enumerate(file_array):
-            if segment is not None:
-                file.write(segment)
-            else:
-                data = b"dkjasbda"
-                hash_ = b"dasjbadskd"
-                while md5(data).digest() != hash_:
-                    clientSocket.sendto(f"GET {filename}/{index}".encode(), ADDR)
-                    message, addr = clientSocket.recvfrom(int(buffer_size))
-                    hash_ = message[hash_init:hash_end]
-                    data = message[hash_end + 1 :]
-                file.write(data)
-        file.close()
-        print("Arquivo transferido com sucesso!")
+        if n_packets is not None:
+            file_array = [None for i in range(int(n_packets))]
 
-clientSocket.close()
+            n_digits = len(str(n_packets))
+            hash_init = n_digits + 1
+            hash_end = hash_init + 16
+            for packet in buffer:
+                header = packet[0:n_digits]
+                hash_ = packet[hash_init:hash_end]
+                data = packet[hash_end + 1 :]
+
+                if md5(data).digest() == hash_:
+                    file_array[int(header)] = data
+
+            file = open(filename, "wb")
+            for index, segment in enumerate(file_array):
+                if segment is not None:
+                    file.write(segment)
+                else:
+                    data = b"dkjasbda"
+                    hash_ = b"dasjbadskd"
+                    while md5(data).digest() != hash_:
+                        clientSocket.sendto(f"GET {filename}/{index}".encode(), ADDR)
+                        message, addr = clientSocket.recvfrom(int(buffer_size))
+                        hash_ = message[hash_init:hash_end]
+                        data = message[hash_end + 1 :]
+                    file.write(data)
+            file.close()
+            print("Arquivo transferido com sucesso!")
+
+    clientSocket.close()
+except TimeoutError:
+    print(
+        "Houve um erro de comunicação entre o servidor e o cliente. Tente novamente mais tarde."
+    )
+except:
+    print("Erro!")
