af4b46db837a469de2575d185dee181bcf74e081
doglas <79492860+DoglasRocha@users.noreply.github.com>
2024-04-27 23:31:44 -0300

    finalizar trabalho 3

--- web_server/app.py
+++ web_server/app.py
@@ -2,6 +2,7 @@ import logging
 from socket import socket, AF_INET, SOCK_STREAM
 from typing import Callable
 from threading import Thread
+import os
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
@@ -35,11 +36,35 @@ class App:
 
         return decorator
 
-    def _404_handler(self) -> None:
-        pass
+    def _404_handler(self, client_socket: socket, addr: str) -> None:
+        client_socket.send(b"HTTP/1.1 404 NOT FOUND\r\nServer: Doglas Rocha\r\n")
 
-    def method_not_allowed_handler(self) -> None:
-        pass
+        with open("./pages/404.html", "rb") as file:
+            while data := file.read(1024):
+                client_socket.send(data)
+
+    def send_image(self, client_socket: socket, addr: str, route: str) -> None:
+        image = route.split("/")[-1]
+
+        if not os.path.isfile(os.path.join("./assets", image)):
+            client_socket.send(
+                b"HTTP/1.1 404 NOT FOUND\r\nServer: Doglas Rocha\r\n\r\n"
+            )
+
+        header = b"HTTP/1.1 200 OK\r\nServer: Doglas Rocha\r\nContent-Type: image/jpeg\r\n\r\n"
+        client_socket.send(header)
+        with open(os.path.join("./assets", image), "rb") as file:
+            while data := file.read(1024):
+                client_socket.send(data)
+
+    def send_page(
+        self, client_socket: socket, addr: str, method: str, route: str
+    ) -> None:
+        header = b"HTTP/1.1 200 OK\r\nServer: Doglas Rocha\r\n\r\n"
+        client_socket.send(header)
+        data = self.__binds[method][route]()
+        for piece in data:
+            client_socket.send(piece)
 
     def handle_requests(self, client_socket: socket, addr: str) -> None:
         logger.info(f"Aceitando conexão com: {addr}")
@@ -51,19 +76,19 @@ class App:
         method = tokens[0]
         route = tokens[1]
 
+        if route.endswith(".jpeg") or route.endswith(".png") or route.endswith(".jpg"):
+            self.send_image(client_socket, addr, route)
+            return
+
         if route not in self.__routes:
-            self._404_handler()
+            self._404_handler(client_socket, addr)
             return
 
         if method not in self.__methods or route not in self.__binds[method]:
-            self.method_not_allowed_handler()
+            self._404_handler(client_socket, addr)
             return
 
-        header = b"HTTP/1.1 200 OK\r\nServer: oiiiikkkkk\r\n"
-        client_socket.send(header)
-        data = self.__binds[method][route]()
-        for piece in data:
-            client_socket.send(data)
+        self.send_page(client_socket, addr, method, route)
 
     def run(self) -> None:
         # print(f"{self.__routes=}", f"{self.__methods=}", f"{self.__binds}", sep="\n")
--- web_server/server.py
+++ web_server/server.py
@@ -1,161 +1,28 @@
-from socket import socket, AF_INET, SOCK_STREAM
-from random import randint
-from time import sleep
-from threading import Thread
-from hashlib import md5
-from typing import Any
-from math import ceil, floor
-import logging
-import os
+from app import App
+from socket import socket
+
+server = App()
 
 
-logger = logging.getLogger(__name__)
-logging.basicConfig(
-    filename="server.log",
-    encoding="utf-8",
-    level=logging.INFO,
-    format="%(levelname)s - %(asctime)s: %(message)s",
-)
-
-
-def chat(client_socket: socket, addr_: str) -> None:
-    client_socket.send(b"OK")
-
-    print("\n")
-    client_message_bytes: bytes = client_socket.recv(1024)
-    print(f"{addr_} diz:\n\t> {client_message_bytes.decode()}")
-
-    response = input("O que você deseja dizer ao cliente?\n\t> ")
-    client_socket.send(response.encode())
-    print("\n")
-
-
-def get_file_checksum(filename: str) -> str:
-    checksum = md5()
-    with open(os.path.join("./files", filename), "rb") as file:
-        while data := file.read(8192):
-            checksum.update(data)
-
-    return checksum.hexdigest()
-
-
-def send_full_file(returnSocket: socket, filename: str, addr_: str) -> None:
-    if not os.path.isfile(os.path.join("./files", filename)):
-        returnSocket.send("ERROR Arquivo não encontrado".encode())
-        return
-
-    logger.info(f"Mandando arquivo {filename} para {addr_}")
-    n_packets: int = ceil(os.path.getsize(os.path.join("./files", filename)) / 1024)
-    n_digits: int = len(str(n_packets))
-    checksum: str = get_file_checksum(filename)
-
-    returnSocket.send(
-        f"OK {n_packets} {n_digits+1+16+1+1024} {filename} {checksum}".encode()
-    )
-    confirmation: bytes = returnSocket.recv(1024)
-
-    with open(os.path.join("./files", filename), "rb") as file:
-        i = 0
+@server.set_route("/", "GET")
+def index():
+    with open("./pages/index.html", "rb") as file:
         while data := file.read(1024):
-            hash_ = md5(data).digest()
-            returnSocket.send(b" ".join([f"{i:{'0'}{n_digits}}".encode(), hash_, data]))
-            while returnSocket.recv(1024) == b"NOK":
-                returnSocket.send(
-                    b" ".join([f"{i:{'0'}{n_digits}}".encode(), hash_, data])
-                )
-            i += 1
+            yield data
 
 
-def send_file(clientSocket: socket, addr_: str) -> bool:
-    logger.info(f"Mandando arquivo para {addr_}")
-    available_files: list[str] = os.listdir("./files")
-
-    # sends file names
-    long_string: str = "".join(
-        f"{index}) {name}\n" for index, name in enumerate(available_files)
-    )
-
-    # sends packet count
-    n_packets: int = ceil(len(long_string) / 1024)
-    clientSocket.send(f"{n_packets}".encode())
-
-    # waits confirmation
-    confirmation: bytes = clientSocket.recv(1024)
-    if confirmation != b"OK":
-        return close_connection(clientSocket, addr_)
-
-    for i in range(n_packets):
-        clientSocket.send(long_string[1024 * i : 1024 * (i + 1)].encode())
-
-    # gets desired file from client
-    file_request: bytes = clientSocket.recv(1024)
-
-    n_file = int(file_request.decode().split(" ")[-1])
-    send_full_file(
-        clientSocket,
-        (
-            ""
-            if n_file >= len(available_files) or n_file < 0
-            else available_files[n_file]
-        ),
-        addr_,
-    )
+@server.set_route("/spaceship", "GET")
+def oi():
+    with open("./pages/spaceship.html", "rb") as file:
+        while data := file.read(1024):
+            yield data
 
 
-def close_connection(clientSocket: socket, addr_: str) -> bool:
-    logger.info(f"Fechando conexão com {addr_}")
-    clientSocket.send(b"FECHADO")
-    clientSocket.close()
-
-    return True
-
-
-def handle_request(clientSocket: socket, addr_: str) -> None:
-    logger.info(f"Aceitando conexão com endereço: {addr_}")
-    while True:
-        message = clientSocket.recv(1024)
-        request = message.decode().split()
-
-        if len(request) <= 0:
-            logger.warning(f"Má requsição de {addr_}")
-            clientSocket.send("ERROR Má requisição".encode())
-            clientSocket.send(b"FECHADO")
-            clientSocket.close()
-            return
-
-        operation: str = request[0]
-        operation_opts: dict = {
-            "SAIR": close_connection,
-            "ARQUIVO": send_file,
-            "CHAT": chat,
-        }
-        if operation not in operation_opts.keys():
-            clientSocket.send("ERROR Método não permitido".encode())
-            clientSocket.send(b"FECHADO")
-            clientSocket.close()
-            return
-
-        if operation_opts[operation](clientSocket, addr_):
-            return
+@server.set_route("/hireus", "GET")
+def kkk():
+    with open("./pages/hire_us.html", "rb") as file:
+        while data := file.read(1024):
+            yield data
 
 
-def run_server() -> None:
-    NAME = "127.0.0.1"
-    PORT = 8065
-
-    serverSocket = socket(AF_INET, SOCK_STREAM)
-    serverSocket.bind((NAME, PORT))
-
-    serverSocket.listen()
-    logger.info(f"Socket escutando em {NAME}:{PORT}")
-    print(f"Socket escutando em {NAME}:{PORT}")
-
-    while True:
-        clientSocket, addr = serverSocket.accept()
-
-        thread = Thread(target=handle_request, args=(clientSocket, addr), daemon=True)
-        thread.start()
-
-
-if __name__ == "__main__":
-    run_server()
+server.run()
